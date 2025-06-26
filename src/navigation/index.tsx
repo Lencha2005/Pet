50b4699409d8a1bc8e59be955a04d0deaaf1106d
Lena_Bondar <lencha.2005@gmail.com>
2025-06-26 15:58:35 +0300

    add ErrorBoundary

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -5,6 +5,7 @@ import {ScreenNames} from '../constants/screenNames';
 import {RootStackNavigation} from './types';
 import LoggedInStack from './LoggedInStack';
 import LoggedOutStack from './LoggedOutStack';
+import ErrorBoundary from '../common/components/ErrorBoundary';
 
 const Stack = createNativeStackNavigator<RootStackNavigation>();
 
@@ -18,18 +19,20 @@ export default function RootNavigation() {
           background: '#FAFAFA',
         },
       }}>
-      <Stack.Navigator
-        initialRouteName={ScreenNames.LOGGED_IN_STACK}
-        screenOptions={{headerShown: false}}>
-        <Stack.Screen
-          name={ScreenNames.LOGGED_IN_STACK}
-          component={LoggedInStack}
-        />
-        <Stack.Screen
-          name={ScreenNames.LOGGED_OUT_STACK}
-          component={LoggedOutStack}
-        />
-      </Stack.Navigator>
+      <ErrorBoundary>
+        <Stack.Navigator
+          initialRouteName={ScreenNames.LOGGED_IN_STACK}
+          screenOptions={{headerShown: false}}>
+          <Stack.Screen
+            name={ScreenNames.LOGGED_IN_STACK}
+            component={LoggedInStack}
+          />
+          <Stack.Screen
+            name={ScreenNames.LOGGED_OUT_STACK}
+            component={LoggedOutStack}
+          />
+        </Stack.Navigator>
+      </ErrorBoundary>
     </NavigationContainer>
   );
 }
